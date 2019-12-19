e0bea4676e0429f4d66ea56f2107844ecd4ed218
mhossomi <mhossomi@daitan.com>
2019-12-19 15:00:17 -0300

    Read test configuration from file.

--- src/test/java/com/bandwidth/voice/quartz/couchbase/integration/IntegrationTestBase.java
+++ src/test/java/com/bandwidth/voice/quartz/couchbase/integration/IntegrationTestBase.java
@@ -1,9 +1,11 @@
 package com.bandwidth.voice.quartz.couchbase.integration;
 
+import static java.lang.String.format;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 import com.bandwidth.voice.quartz.couchbase.CouchbaseJobStore;
 import com.couchbase.client.java.CouchbaseCluster;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -25,6 +27,19 @@ import org.quartz.simpl.SimpleThreadPool;
 @Slf4j
 public class IntegrationTestBase {
 
+    private static Properties properties;
+
+    static {
+        try {
+            properties = new Properties();
+            properties.load(Optional.of(IntegrationTestBase.class.getClassLoader())
+                    .map(loader -> loader.getResourceAsStream("config.properties"))
+                    .orElseThrow(() -> new RuntimeException("Could not find config.properties")));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load properties", e);
+        }
+    }
+
     @Rule
     public TestName testName = new TestName();
     @Rule
@@ -37,26 +52,28 @@ public class IntegrationTestBase {
     @BeforeClass
     public static void setupCouchbase() {
         CouchbaseJobStore.setCluster(CouchbaseCluster
-                .create("docker:8091")
-                .authenticate("Administrator", "password"));
+                .create(properties.getProperty("couchbase.endpoint"))
+                .authenticate(
+                        properties.getProperty("couchbase.username"),
+                        properties.getProperty("couchbase.password")));
     }
 
     @Before
     public void setup() throws SchedulerException {
         // Hopefully random enough
-        alias = String.format("%08x", UUID.randomUUID().getLeastSignificantBits());
+        alias = format("%08x", UUID.randomUUID().getLeastSignificantBits());
         log.info("Test {}.{} alias: {}", getClass().getSimpleName(), testName.getMethodName(), alias);
 
-        Properties properties = new Properties();
-        properties.put("org.quartz.scheduler.instanceName", getClass().getSimpleName() + "." + alias);
-        properties.put("org.quartz.scheduler.threadName", getClass().getSimpleName() + "." + alias);
-        properties.put("org.quartz.scheduler.idleWaitTime", "1000");
-        properties.put("org.quartz.jobStore.class", CouchbaseJobStore.class.getName());
-        properties.put("org.quartz.threadPool.class", SimpleThreadPool.class.getName());
-        properties.put("org.quartz.threadPool.threadCount", "3");
-        properties.put("org.quartz.jobStore.bucketName", "quartz");
+        Properties schedulerProperties = new Properties();
+        schedulerProperties.put("org.quartz.scheduler.instanceName", getClass().getSimpleName() + "." + alias);
+        schedulerProperties.put("org.quartz.scheduler.threadName", getClass().getSimpleName() + "." + alias);
+        schedulerProperties.put("org.quartz.scheduler.idleWaitTime", "1000");
+        schedulerProperties.put("org.quartz.jobStore.class", CouchbaseJobStore.class.getName());
+        schedulerProperties.put("org.quartz.threadPool.class", SimpleThreadPool.class.getName());
+        schedulerProperties.put("org.quartz.threadPool.threadCount", "3");
+        schedulerProperties.put("org.quartz.jobStore.bucketName", properties.getProperty("couchbase.bucket"));
 
-        SchedulerFactory factory = new StdSchedulerFactory(properties);
+        SchedulerFactory factory = new StdSchedulerFactory(schedulerProperties);
         scheduler = factory.getScheduler();
         scheduler.start();
     }
